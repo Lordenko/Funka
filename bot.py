cdb7601dd7b3227bd70e17df39bea20b57630279
Lordenko <gameryt527@gmail.com>
2025-03-05 19:40:44 +0200

    regular commit

--- bot.py
+++ bot.py
@@ -1,18 +1,33 @@
 import disnake
 from disnake.ext import commands
 import json
+import asyncio
 
 from utils.defs import load_cogs
 from data.config import token
 
 intents = disnake.Intents.all()
 
-bot = commands.InteractionBot(intents=intents)
 
+bot = commands.InteractionBot(intents=intents)
 load_cogs(bot, 'cogs.commands', 'cogs.events')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is online!')
+
+    activities = [
+        disnake.Activity(type=disnake.ActivityType.watching, name="to broken code."),
+        disnake.Activity(type=disnake.ActivityType.watching, name="to broken code.."),
+        disnake.Activity(type=disnake.ActivityType.watching, name="to broken code..."),
+        disnake.Activity(type=disnake.ActivityType.watching, name="to broken code.."),
+
+    ]
+
+    while True:
+        for activity in activities:
+            await bot.change_presence(activity=activity)
+            await asyncio.sleep(2)
+
 
 bot.run(token)
