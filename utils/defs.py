2cfc2e5f5de3f7381142358256ad3e832ef4fae2
ipz234_bdl <ipz234_bdl@student.ztu.edu.ua>
2025-03-07 22:17:51 +0200

    micro fix

--- utils/defs.py
+++ utils/defs.py
@@ -1,3 +1,7 @@
 def load_cogs(bot, *initial_extensions):
     for extension in initial_extensions:
         bot.load_extension(extension)
+
+async def vc_disconnect(vc):
+    asyncio.sleep(3)
+    await vc.disconnect()
